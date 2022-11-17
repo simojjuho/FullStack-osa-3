c7bd47f0b61c931b8a3569c5b5c5789e5ca99e6e
Juho Simojoki <juho.simojoki@outlook.com>
2022-11-17 12:51:41 +0200

    t 3.8

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
 const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const time = new Date
 const app = express()
 app.use(express.json())
+morgan.token('bodyJSON', function (request) {
+  console.log(request.body)
+  return JSON.stringify(request.body)
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bodyJSON'))
 
 let persons =
-    [
-      {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      }
-    ]
-  
+  [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    }
+  ]
+
+
 const info = () => {
   return (
     `<p>Phonebook has info for ${persons.length} people.</p><br />${time.toString()}`
   )
 }
-  
+
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-    console.log("GET ok")
+  res.send('<h1>Hello World!</h1>')
+  console.log("GET ok")
 })
 
 app.get('/info', (req, res) => {
@@ -46,7 +54,9 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+  console.log(res.body)
+  console.log(req.body)
+  res.json(persons)
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -54,11 +64,11 @@ app.get('/api/persons/:id', (request, response) => {
   const person = persons.find(pers => pers.id === id)
 
   if (person) {
-    response.json(person)  
-  } 
-    else {    
-      response.status(404).end()  
-    }
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -69,11 +79,11 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-  const id = Math.floor(10000*Math.random())
+  const id = Math.floor(10000 * Math.random())
   const contact = request.body
-  if (!contact.number) return response.status(400).json({error: 'number missing!'}) 
-  if (!contact.name) return response.status(400).json({error: 'name missing!'})
-  if (persons.some(contact => contact.name === contact.name)) return response.status(400).json({error: 'Name exists already!'})
+  if (!contact.number) return response.status(400).json({ error: 'number missing!' })
+  if (!contact.name) return response.status(400).json({ error: 'name missing!' })
+  if (persons.some(person => person.name === contact.name)) return response.status(400).json({ error: 'Name exists already!' })
   contact.id = id
   persons = persons.concat(contact)
   response.json(contact)
@@ -82,5 +92,5 @@ app.post('/api/persons', (request, response) => {
 
 const PORT = 3001
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
