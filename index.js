96e5b3a2d9f8800a3b2ca97f73daa1073aa0456a
Juho Simojoki <juho.simojoki@outlook.com>
2022-11-17 19:39:22 +0200

    3.9-3.10 tehtävät: Puhelinluettelo toimii portissa 3001 paikallisesti ja netissä myös osoitteessa https://winter-sea-4186.fly.dev/

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
