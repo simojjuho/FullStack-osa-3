7f99ccaf2e9c683a5f7fab92e66c16503572bf46
Juho Simojoki <juho.simojoki@outlook.com>
2022-11-22 11:37:03 +0200

    3.12-3.18 tehtävät tehty. Sovelluksen front ja back toimivat kotikoneella että internetissä.

--- models/contact.js
+++ models/contact.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-
 const url = process.env.MONGODB_URI
 
 console.log('Connecting to', url )
@@ -17,7 +12,7 @@ mongoose.connect(url)
         console.log('error connecting to MongoDB', error.message)
     })
 
-//`mongodb+srv://js-fullstack:${password}@hyfs22.lrperjl.mongodb.net/contactListApp?retryWrites=true&w=majority`
+
 
 const contactSchema = new mongoose.Schema({
     name: String,
